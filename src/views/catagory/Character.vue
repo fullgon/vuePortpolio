589389b40519630d4f58131ab1fbe9a2b1467907
fullgon <mbj0119@gmail.com>
2022-10-16 08:34:42 +0900

    추가 : 종이재질 이미지 추가 및 인물 페이지 배경으로 적용

---
+++ src/views/catagory/Character.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container" style="background-image: url(image/paper.jpg)"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center center;
+}
+</style>
