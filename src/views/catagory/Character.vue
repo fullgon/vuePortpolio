822d9e3712b4dc5707da886136a7ac6eb63b4382
fullgon <mbj0119@gmail.com>
2022-10-16 10:15:10 +0900

    추가 : 이미지 틀 생성

--- src/views/catagory/Character.vue
+++ src/views/catagory/Character.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="container" style="background-image: url(image/paper.jpg)"></div>
+  <div class="container" style="background-image: url(image/headerPaper.jpg)">
+    <v-layout class="image-container" wrap>
+      <div class="image-box">
+        <img class="img" src="image/top1.jpg" />
+      </div>
+
+      <div class="image-box-right">
+        <img class="img" src="image/headerPaper.jpg" />
+      </div>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -8,9 +18,35 @@ export default {};
 
 <style scoped lang="less">
 .container {
-  width: 100vw;
-  height: 100vh;
+  width: fill;
+  height: fill;
   background-size: cover;
   background-position: center center;
+}
+
+.image-container {
+  width: fill;
+  padding: 2% 5% 0% 5%;
+  border: solid;
+  display: flex;
+}
+
+.image-box {
+  width: 60%;
+  padding: 5%;
+  border: solid;
+}
+
+.image-box-right {
+  width: 60%;
+  padding: 5%;
+  border: solid;
+  margin-left: 40%;
+}
+
+.img {
+  width: 100%;
+  height: 100%;
+  border: solid white;
 }
 </style>
